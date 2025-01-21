450988c3d175fbcaffc52447285f2414944eca69
crodic <alice01422@gmail.com>
2025-01-21 09:00:33 +0700

    Add mobile menu toggle functionality and responsive styles

--- src/js/main.ts
+++ src/js/main.ts
@@ -3,3 +3,11 @@ import '../sass/style.scss';
 // Components
 import '../sass/components/ui/button.scss';
 import '../sass/components/ui/card.scss';
+
+const toggleButton: HTMLDivElement = document.querySelector('.header__menu') as HTMLDivElement;
+const mobileMenu: HTMLDivElement = document.querySelector('.header__mobile') as HTMLDivElement;
+
+toggleButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    console.log('Test');
+});
